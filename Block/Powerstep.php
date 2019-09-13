414e5324c29adf863a968272200095bd60b9f8ed
Casper Kromann Nielsen <ckn@clerk.io>
2019-09-13 15:51:05 +0200

    Added version turn and generator for recommendation

--- Block/Powerstep.php
+++ Block/Powerstep.php
@@ -63,4 +63,18 @@ class Powerstep extends AbstractProduct
 
         return (array) $templates;
     }
+    
+    public function generateRandomString($length = 25) {
+
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+
+    }
 }
--- Block/PowerstepPopup.php
+++ Block/PowerstepPopup.php
@@ -149,4 +149,18 @@ class PowerstepPopup extends Template
 
         return (array) $templates;
     }
+
+    public function generateRandomString($length = 25) {
+        
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        
+        return $randomString;
+        
+    }
 }
