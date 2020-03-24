a2fd840ade437e092170ffedf9dd2d7c7e61a023
Casper Kromann Nielsen <ckn@clerk.io>
2020-03-24 19:07:55 +0100

    Live Search CSS Selector

--- Block/LiveSearch.php
+++ Block/LiveSearch.php
@@ -24,29 +24,37 @@ class LiveSearch extends Template
      */
     public function shouldIncludeCategories()
     {
-        return ($this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_INCLUDE_CATEGORIES)) ? 'true' : 'false';
+        return ($this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_INCLUDE_CATEGORIES, \Magento\Store\Model\ScopeInterface::SCOPE_STORE)) ? 'true' : 'false';
     }
     public function getSuggestions()
     {
-        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_SUGGESTIONS);
+        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_SUGGESTIONS, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
     public function getCategories()
     {
-        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_CATEGORIES);
+        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_CATEGORIES, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
     public function getPages()
     {
-        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_PAGES);
+        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_PAGES, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
     public function getPagesType()
     {
-        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_PAGES_TYPE);
+        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_PAGES_TYPE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
     public function getDropdownPosition()
     {
-        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_DROPDOWN_POSITION);
+        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_DROPDOWN_POSITION, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+    public function getInputSelector()
+    {
+        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_INPUT_SELECTOR, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+    public function getFormSelector()
+    {
+        return $this->_scopeConfig->getValue(Config::XML_PATH_LIVESEARCH_FORM_SELECTOR, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 }
