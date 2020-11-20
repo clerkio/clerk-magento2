3f8d722ef575dd22371171697a3de2ec341c9580
Casper Kromann Nielsen <ckn@clerk.io>
2020-11-20 14:53:53 +0100

    Handle array_combine bug on search page

--- Block/Result.php
+++ Block/Result.php
@@ -50,29 +50,32 @@ class Result extends BaseResult
         ];
 
         if ($this->_scopeConfig->isSetFlag(Config::XML_PATH_FACETED_SEARCH_ENABLED, ScopeInterface::SCOPE_STORE)) {
-            $spanAttributes['data-facets-target'] = "#clerk-search-filters";
+            try {
+                $spanAttributes['data-facets-target'] = "#clerk-search-filters";
 
-            if ($titles = $this->_scopeConfig->getValue(Config::XML_PATH_FACETED_SEARCH_TITLES, ScopeInterface::SCOPE_STORE)) {
-                $titles = json_decode($titles, true);
+                if ($titles = $this->_scopeConfig->getValue(Config::XML_PATH_FACETED_SEARCH_TITLES, ScopeInterface::SCOPE_STORE)) {
+                    $titles = json_decode($titles, true);
 
-                // sort alphabetically by name
-                uasort($titles, function($a, $b) {
-                    if (isset($a['sort_order']) && isset($b['sort_order'])) {
-                        return $a['sort_order'] > $b['sort_order'];
+                    // sort alphabetically by name
+                    uasort($titles, function($a, $b) {
+                        if (isset($a['sort_order']) && isset($b['sort_order'])) {
+                            return $a['sort_order'] > $b['sort_order'];
+                        }
+
+                        return true;
+                    });
+
+                    $spanAttributes['data-facets-titles'] = json_encode(array_filter(array_combine(array_keys($titles), array_column($titles, 'label'))));
+                    $spanAttributes['data-facets-attributes'] = json_encode(array_keys($titles));
+
+                    if ($multiselectAttributes = $this->_scopeConfig->getValue(Config::XML_PATH_FACETED_SEARCH_MULTISELECT_ATTRIBUTES, ScopeInterface::SCOPE_STORE)) {
+                        $spanAttributes['data-facets-multiselect-attributes'] = '["' . str_replace(',', '","', $multiselectAttributes) . '"]';
                     }
-
-                    return true;
-                });
-
-                $spanAttributes['data-facets-titles'] = json_encode(array_filter(array_combine(array_keys($titles), array_column($titles, 'label'))));
-                $spanAttributes['data-facets-attributes'] = json_encode(array_keys($titles));
-
-                if ($multiselectAttributes = $this->_scopeConfig->getValue(Config::XML_PATH_FACETED_SEARCH_MULTISELECT_ATTRIBUTES, ScopeInterface::SCOPE_STORE)) {
-                    $spanAttributes['data-facets-multiselect-attributes'] = '["' . str_replace(',', '","', $multiselectAttributes) . '"]';
                 }
+            } catch (\Exception $e) {
+                $spanAttributes['data-facets-attributes'] = '["price","categories"]';
             }
         }
-
 
         foreach ($spanAttributes as $attribute => $value) {
             $output .= ' ' . $attribute . '=\'' . $value . '\'';
