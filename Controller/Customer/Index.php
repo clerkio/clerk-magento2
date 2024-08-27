3202d6b0b99f6700f5325824bac8c5893ed89233
Stubbe <abs@clerk.io>
2024-08-27 15:39:57 +0200

    make comparison of customer id and subscriber customer id for status

--- Controller/Customer/Index.php
+++ Controller/Customer/Index.php
@@ -161,7 +161,7 @@ class Index extends AbstractAction
                     if ($this->scopeConfig->getValue(Config::XML_PATH_SUBSCRIBER_SYNCHRONIZATION_ENABLED, $this->scope, $this->scopeid)) {
                         $sub_state = $subscriberInstance->loadByEmail($customer['email']);
                         if ($sub_state->getId()) {
-                            $_customer['subscribed'] = (bool) ($sub_state->getSubscriberStatus() == 1);
+                            $_customer['subscribed'] = (bool) ($sub_state->getSubscriberStatus() == 1 && $sub_state->getCustomerId() == $customer['id']);
                         } else {
                             $_customer['subscribed'] = false;
                         }
@@ -181,7 +181,7 @@ class Index extends AbstractAction
                             $_sub = array();
                             $_sub['id'] = 'SUB' . $subscriber['subscriber_id'];
                             $_sub['email'] = $subscriber['subscriber_email'];
-                            $_sub['subscribed'] = (bool) ($subscriber['subscriber_status'] == 1);
+                            $_sub['subscribed'] = (bool) ($subscriber['subscriber_status'] == 1 && $sub_state->getSubscriberEmail() == $subscriber['subscriber_email']);
                             $_sub['name'] = "";
                             $_sub['firstname'] = "";
                             $_sub['unsub_url'] = $sub_state->getUnsubscriptionLink();
