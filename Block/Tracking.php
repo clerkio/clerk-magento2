642a325e138eb037ce844dee734b694144d8b99f
Alexander Stage <abs@stubbe.dev>
2024-11-19 14:58:13 +0100

    add namespace escape to exception

--- Block/Tracking.php
+++ Block/Tracking.php
@@ -53,7 +53,7 @@ class Tracking extends Template
         try {
             $customerData = $this->_customerSession->getCustomer();
             $email = $customerData->getEmail();
-        } catch (Exception) {
+        } catch (\Exception $e) {
         }
         return $email;
     }
